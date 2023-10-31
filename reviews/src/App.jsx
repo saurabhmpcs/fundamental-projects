ed7ca5277c01ff9083cca2e3bb3b1965f1540ae4
Saurabh Singh <saurabh.rgt@gmail.com>
2023-10-31 23:47:29 +0530

    random user

--- reviews/src/App.jsx
+++ reviews/src/App.jsx
@@ -1,4 +1,68 @@
+import { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { job, name, image, text } = people[index];
+
+  const checkNumber = (number) => {
+    if (number > people.length - 1) {
+      return 0;
+    }
+    if (number < 0) {
+      return people.length - 1;
+    }
+    return number;
+  };
+
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      return checkNumber(newIndex);
+    });
+  };
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const randomPerson = () => {
+    let randomNumer = Math.floor(Math.random() * people.length);
+
+    if (randomNumer === index) {
+      randomNumer = index + 1;
+    }
+    setIndex(checkNumber(randomNumer));
+  };
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img className="person-img" src={image} alt={name} />
+          <span className="qoute-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className="next-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button className="btn btn-hipster" onClick={randomPerson}>
+          Surprise Me!
+        </button>
+      </article>
+    </main>
+  );
 };
 export default App;
