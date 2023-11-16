0f7984019818d54b146eaeb614e61d5281a9f8e6
Saurabh Singh <saurabh.rgt@gmail.com>
2023-11-16 21:47:51 +0530

    tabs final-commit

--- Tabs/src/App.jsx
+++ Tabs/src/App.jsx
@@ -1,6 +1,44 @@
-const url = 'https://course-api.com/react-tabs-project';
+import { useEffect, useState } from "react";
+import JobInfo from "./JobInfo";
+import BtnContainer from "./BtnContainer";
+
+const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  const [currentItem, setCurrentItem] = useState(1);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className="jobs-center">
+        <div className="loading"></div>
+      </section>
+    );
+  }
+
+  return (
+    <section className="jobs-center">
+      {/* {button container} */}
+      <BtnContainer
+        jobs={jobs}
+        setCurrentItem={setCurrentItem}
+        currentItem={currentItem}
+      />
+      {/* {job info} */}
+      <JobInfo jobs={jobs} currentItem={currentItem} />
+    </section>
+  );
 };
 export default App;
