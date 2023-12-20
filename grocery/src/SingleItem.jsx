10645c6463966453829f915f75377d7f1a2ba3ef
Saurabh Singh <saurabh.rgt@gmail.com>
2023-12-20 19:34:21 +0530

    grocery-bud

--- grocery/src/SingleItem.jsx
+++ grocery/src/SingleItem.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 
-const SingleItem = () => {
-  return <div>SingleItem</div>;
+const SingleItem = ({ item, removeItem }) => {
+  const [isChecked, setIsChecked] = useState(item.completed);
+
+  return (
+    <div className="single-item">
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setIsChecked(!isChecked)}
+      />
+      <p
+        style={{
+          textTransform: "capitalize",
+          textDecoration: isChecked && "line-through",
+        }}
+      >
+        {item.name}
+      </p>
+      <button
+        className="btn remove-btn"
+        type="button"
+        onClick={() => removeItem(item.id)}
+      >
+        delete
+      </button>
+    </div>
+  );
 };
 
 export default SingleItem;
