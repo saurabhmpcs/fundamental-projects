dce18d0c7589af58ef901c56fef2d671e9cc99e4
Saurabh Singh <saurabh.rgt@gmail.com>
2023-12-23 17:32:53 +0530

    final-commit-grocery

--- grocery/src/SingleItem.jsx
+++ grocery/src/SingleItem.jsx
@@ -1,19 +1,17 @@
 import React, { useState } from "react";
 
-const SingleItem = ({ item, removeItem }) => {
-  const [isChecked, setIsChecked] = useState(item.completed);
-
+const SingleItem = ({ item, removeItem, editItem }) => {
   return (
     <div className="single-item">
       <input
         type="checkbox"
-        checked={isChecked}
-        onChange={() => setIsChecked(!isChecked)}
+        checked={item.completed}
+        onChange={() => editItem(item.id)}
       />
       <p
         style={{
           textTransform: "capitalize",
-          textDecoration: isChecked && "line-through",
+          textDecoration: item.completed && "line-through",
         }}
       >
         {item.name}
